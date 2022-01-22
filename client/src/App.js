f9f7971d54f1f44df6da62195f97567990674ef2
Gabriel Freire <gabrielfvale@gmail.com>
2022-01-22 09:09:26 -0300

    Finish frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Button, Chip } from "@mui/material";
+import { Box, Button, Chip, Stack } from "@mui/material";
 
 import AlarmLightOutline from "mdi-material-ui/AlarmLightOutline";
 import AlarmLightOffOutline from "mdi-material-ui/AlarmLightOffOutline";
@@ -7,13 +7,23 @@ import Radiator from "mdi-material-ui/Radiator";
 import RadiatorOff from "mdi-material-ui/RadiatorOff";
 import LightbulbOnOutline from "mdi-material-ui/LightbulbOnOutline";
 import LightbulbOffOutline from "mdi-material-ui/LightbulbOffOutline";
+import { Thermometer, Home, Plus, Minus } from "mdi-material-ui";
 
 const socket = new WebSocket("ws://localhost:8000/ws");
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
-  const [message, setMessage] = useState("");
   const [actuatorOn, setActuatorOn] = useState([false, false, false]);
+  const [temperature, setTemperature] = useState(0);
+
+  const sendAction = (key, action) => {
+    socket.send(
+      JSON.stringify({
+        actuatorType: key,
+        commandKey: action,
+      })
+    );
+  };
 
   const toggleActuator = (key, index) => {
     const prev = [...actuatorOn];
@@ -26,13 +36,7 @@ function App() {
       actuatorType: key,
       commandKey: newValue ? "TurnOn" : "TurnOff",
     });
-
-    socket.send(
-      JSON.stringify({
-        actuatorType: key,
-        commandKey: newValue ? "TurnOn" : "TurnOff",
-      })
-    );
+    sendAction(key, newValue ? "TurnOn" : "TurnOff");
   };
 
   const actuators = [
@@ -45,7 +49,7 @@ function App() {
     {
       name: "Heater",
       actuatorType: "heater",
-      actions: ["TurnOn", "TurnOff"],
+      actions: ["TurnOn", "TurnOff", "RaiseTemp", "LowerTemp"],
       icons: { on: Radiator, off: RadiatorOff },
     },
     {
@@ -58,13 +62,17 @@ function App() {
 
   useEffect(() => {
     socket.onopen = () => {
-      setMessage("Connected");
+      // setMessage("Connected");
       setIsConnected(true);
     };
 
     socket.onmessage = (e) => {
-      console.log("message");
-      setMessage("Get message from server: " + e.data);
+      const data = JSON.parse(e.data);
+
+      if (data?.sensor) {
+        setTemperature(data.value);
+      }
+      console.log(data);
     };
 
     return () => {
@@ -73,19 +81,73 @@ function App() {
   }, []);
 
   return (
-    <Box>
-      <Chip
-        label={isConnected ? "CONNECTED" : "NOT CONNECTED"}
-        color={isConnected ? "success" : "error"}
-      />
-      <p>{message}</p>
-      {actuators.map((actuator, index) => (
-        <>
-          <Button onClick={() => toggleActuator(actuator.actuatorType, index)}>
-            {actuatorOn[index] ? <actuator.icons.on /> : <actuator.icons.off />}
-          </Button>
-        </>
-      ))}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#ccc",
+      }}
+    >
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{ display: "flex", alignItems: "center" }}
+      >
+        <Home />
+
+        <h2>Welcome to Home Assistant</h2>
+        <Chip
+          label={isConnected ? "CONNECTED" : "NOT CONNECTED"}
+          color={isConnected ? "success" : "error"}
+        />
+      </Stack>
+
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Thermometer />
+        <p>{temperature} °C</p>
+      </Box>
+
+      <Stack direction="row" spacing={8}>
+        {actuators.map((actuator, index) => (
+          <Stack spacing={2}>
+            <Button
+              variant="contained"
+              startIcon={
+                actuatorOn[index] ? (
+                  <actuator.icons.on />
+                ) : (
+                  <actuator.icons.off />
+                )
+              }
+              onClick={() => toggleActuator(actuator.actuatorType, index)}
+            >
+              {actuator.name}
+            </Button>
+            {actuator.actuatorType === "heater" && (
+              <>
+                <Button
+                  variant="contained"
+                  startIcon={<Plus />}
+                  onClick={() => sendAction(actuator.actuatorType, "RaiseTemp")}
+                >
+                  Increase
+                </Button>
+                <Button
+                  variant="contained"
+                  startIcon={<Minus />}
+                  onClick={() => sendAction(actuator.actuatorType, "LowerTemp")}
+                >
+                  Decrease
+                </Button>
+              </>
+            )}
+          </Stack>
+        ))}
+      </Stack>
     </Box>
   );
 }
